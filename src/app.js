e5ef7dd6d376dd169589df516c5b1747d5334878
Michael Mah <Michael.Mah2@albertahealthservices.ca>
2020-01-21 00:51:44 -0700

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
+const port = process.env.PORT || 3000 // process.env.PORT comes from heroku
 
 // define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -110,6 +110,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + process.env.PORT +'.')
 })
